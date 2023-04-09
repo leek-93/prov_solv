7ecb8949e0102b6aeb86d69dc55298c9da5a758b
leek-93 <squeaksleeks@gmail.com>
2023-04-09 02:16:14 -0500

    day1extra

---
+++ prov_solv/DataType.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/*
+P 11382 take 3 values that are large and sum
+
+*/
+
+
+int main(){
+    long long a,b,c;
+    scanf("%lld %lld %lld", &a, &b, &c);
+    printf("%lld",a+b+c);
+    return 0;
+}
+
+/*
+    int d
+    long ld
+    long long lld
+    unsigned long long llu
+*/
---
+++ prov_solv/MultiplicationCalc.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/*
+P 2588 take 2 integer values thats between 100 and 999 
+and multiply them. each place of second input number should
+multiply first input number and also show total
+*/
+int main(){
+    int a=0; int b=0;
+    int c[2] = {};
+
+    scanf("%d %d",&a,&b);
+    c[0] = b/100; 
+    c[1] = b/10 - (b/100)*10; 
+
+    printf("%d\n", b);
+    c[2] = b%10;
+
+    printf("%d\n", b);
+    /*
+    printf("%d\n", c[2]*a);
+    printf("%d\n", c[1]*a);
+    printf("%d\n", c[0]*a);
+    printf("%d\n", b*a);
+    */
+    return 0;
+    }
