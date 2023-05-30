36acf3b1663f5da7e5239412d39686bf9fe9168e
squeaksleeks <squeaksleeks@gmail.com>
2023-05-30 23:44:19 +0900

    day21

--- prov_solv/ReminderCnt.c
+++ prov_solv/ReminderCnt.c
@@ -6,38 +6,31 @@ return the count of different remainder results
 int main(){
     int input[10];
     int remainder[10];
-    int cnt=0;
-    int val[10];
+    int cnt=0, result =0;
 
     for(int i = 0; i < 10; i++){
         scanf("%d", &input[i]);
+        remainder[i] = input[i]%42;
     }
-
-    val[0] = input[0]%42;
 
     //calc the remainders
     int j = 0;
     while(j < 10){
-        remainder[j] = input[j]%42;
-
-        //search and count
-        //check if remainder is same as value
-        //otherwise check previous from array and add count
-        if(val[cnt] == remainder[j]){
-        }else{
-            for(int i =0; i< 10; i++){
-                if(remainder[j] == val[i]){
-                }else{
-                    val[cnt] = remainder[j];
-                    cnt++;
-                }
-                printf("remainder %d, val %d\n", remainder[j], val[i]);
+        cnt =0;
+        //nested loop for checking every elements
+        //if value matches, count variable goes up.
+        for(int k=j+1; k<10;k++){
+            if(remainder[j] == remainder[k]){
+                cnt++;
             }
+        }
+        if(cnt == 0){
+            result++;
         }
 
         j++;
     }
-    printf("%d", cnt+1);
+    printf("%d", result);
 
     return 0;
 }
