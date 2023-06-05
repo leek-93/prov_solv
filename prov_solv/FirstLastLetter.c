eb9be7e8e3fd0b0cef808f5c599ae42d7f9b5119
squeaksleeks <squeaksleeks@gmail.com>
2023-06-06 01:03:08 +0900

    day26

--- prov_solv/FirstLastLetter.c
+++ prov_solv/FirstLastLetter.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
+#include<string.h>
 /*
 P9086 take the number of cases and each cases as input
 print first and last letters
 */
 int main(){
     int num=0,j=0;
-    char string[100] ={0,};
+    char string[100];
+    char result[32];
     scanf("%d", &num);
-    printf("num is %d\n", num);
     for(int i=0; i<num;i++){
-        do{
-            scanf("%c", &string[j]);
-            printf("string is %d\n", string[j]);
-            j++;
-        }while (string[j-1] != '\n');
-        printf("%d", string[i]);
+        scanf("%s", string);
+        printf("%d", strlen(string));
+        result[j] = string[i];
+        j++;
+        result[j] = string[i+strlen(string)-1];
+        j++;
     }
+    for(int i=0;i<j;i++){
+        printf("%c", result[i]);
+    }
+
     return 0;
 }
