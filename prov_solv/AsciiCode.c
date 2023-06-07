2e2818677048fbabd4d995790c2bc62b5da2374c
squeaksleeks <squeaksleeks@gmail.com>
2023-06-08 00:14:12 +0900

    day28

---
+++ prov_solv/AsciiCode.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+P11654 take a character and return number matching in ascii table
+*/
+int main(){
+    char input;
+    scanf("%c", &input);
+    printf("%d", input);
+    return 0;
+}
