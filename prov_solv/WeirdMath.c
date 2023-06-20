96a6319650cc7da9629da208f6d94a846b8f6945
squeaksleeks <squeaksleeks@gmail.com>
2023-06-20 23:43:27 +0900

    day34

--- prov_solv/WeirdMath.c
+++ prov_solv/WeirdMath.c
@@ -1,21 +1,33 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 /*
 P2908 take two numbers and flip numbers
 for example, 734 will be 437
 print larger number between flipped numbers
 */
 int main(){
-    int inp=0, inp2=0;
+    int inp=0, inp2=0, x=0, y=0;
     char str[10], temp;
     memset(str, 0, sizeof(char)*10);
     scanf("%d %d", &inp, &inp2);
     str[0] = inp%10;
-    str[1] = inp/10 - (inp/100)*10;
     str[2] = inp/100;
+    x=atoi(str[2])*100+atoi(str[1])*10+atoi(str[0]);
+    printf("%d", x);
+
+    str[0] = inp2%10;
+    str[2] = inp2/100;
     temp = str[0];
-    str[0] = str[1];
-    str[1] = str[2];
+    str[0] = str[2];
     str[2] = temp;
+    y=atoi(str);
+
+    if(x>=y){
+        printf("%d", x);
+    }else{
+        printf("%d", y);
+    }
 
     return 0;
 }
--- prov_solv/WordCount.c
+++ prov_solv/WordCount.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 /*
 P1152 take a line of strings and print how many words are contained
 */
 int main(){
     int count=0, i=0;
-    char string[10000001];
-    memset(string, 0, sizeof(char)*10000001);
+    char *string = (char *)malloc(sizeof(char) * 10000001);
     fgets(string, 10000001, stdin);
-    if(string[0] == '\n'){
-        count--;
+    if(string[0] == '\n' || (string[0] == ' ' && string[1] == '\n')){
+        printf("0");
+        return 0;
     }
-    if(string[strlen(string)] == ' '){
+    
+    if(string[0] == ' '){
         count--;
     }
 
+    if(string[strlen(string)-2] == ' '){
+        count--;
+    }
     while(string[i] != '\n'){
         if(string[i] == ' '){
             count++;
@@ -22,5 +27,6 @@ int main(){
         i++;
     }
     printf("%d", count+1);
+    free(string);
     return 0;
 }
