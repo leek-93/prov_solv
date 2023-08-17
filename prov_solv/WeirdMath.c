852df36ee185ef78d4361c2374cf0ebb30ac155e
squeaksleeks <squeaksleeks@gmail.com>
2023-08-16 23:49:41 -0500

    day35

--- prov_solv/WeirdMath.c
+++ prov_solv/WeirdMath.c
@@ -1,33 +1,21 @@
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
 /*
-P2908 take two numbers and flip numbers
-for example, 734 will be 437
+P2908 take two numbers, reverse and compare them
+for example, 734 will get 437
 print larger number between flipped numbers
 */
 int main(){
-    int inp=0, inp2=0, x=0, y=0;
-    char str[10], temp;
-    memset(str, 0, sizeof(char)*10);
-    scanf("%d %d", &inp, &inp2);
-    str[0] = inp%10;
-    str[2] = inp/100;
-    x=atoi(str[2])*100+atoi(str[1])*10+atoi(str[0]);
-    printf("%d", x);
+    int input=0, input2=0, new=0, new2=0;
+    char temp;
+    scanf("%d %d", &input, &input2);
 
-    str[0] = inp2%10;
-    str[2] = inp2/100;
-    temp = str[0];
-    str[0] = str[2];
-    str[2] = temp;
-    y=atoi(str);
+    new=input%10*100+(input/10 - (input/100)*10)*10+input/100;
+    new2=input2%10*100+(input2/10 - (input2/100)*10)*10+input2/100;
 
-    if(x>=y){
-        printf("%d", x);
-    }else{
-        printf("%d", y);
-    }
+    if(new >= new2)
+        printf("%d", new);
+    else
+        printf("%d", new2);
 
     return 0;
 }
