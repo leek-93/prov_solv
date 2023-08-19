8f10e3135320933735f4759860a3b43ac1455730
squeaksleeks <squeaksleeks@gmail.com>
2023-08-19 00:14:22 -0500

    day37

--- prov_solv/DialTime.c
+++ prov_solv/DialTime.c
@@ -2,22 +2,23 @@
 #include<string.h>
 /*
 P5622 Take upper cased string and calculate total time
-1 to 9, each number represent 3 alphabet letters
+1 to 9, each number represent alphabet letters
+2 ABC 3 DEF 4 GHI 5 JKL 6 MNO 7 PWRS 8 TUV 9 WXYZ
 1 takes 2 seconds to dial, 2 takes 3 seconds...
 */
 
 int main(){
-    char input[16];
-    int temp=0, num=0, i=0;
-    memset(input, '0', 16);
+    char input[16], alphabet[26] ={2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9};
+    int temp=0, sec=0, i=0;
+    memset(input, 0, 16);
 
     scanf("%s", input);
     
-    while(input[i+1] != '0'){
-        num += (input[i] - 64)/3;
+    while(input[i] != 0){
+        sec += alphabet[input[i] - 65] + 1;
         i++;
     }
-    printf("%d", num+6);
+    printf("%d", sec);
 
     return 0;
 }
